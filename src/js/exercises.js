6b68273f216b5c827ecc1aa74c6e7c511d726740
yasinskislava <yasinskislava@gmail.com>
2024-05-21 01:05:36 +0300

    exercises modal

--- src/js/exercises.js
+++ src/js/exercises.js
@@ -10,6 +10,8 @@ export default async function exercises() {
         activeFilter.classList.remove("active-filter");
         e.target.classList.add("active-filter");
         activeFilter = e.target;
+        categories.classList.remove("bodyParts");
+        categories.classList.add("muscles");
         renderMuscles();
         renderPages(2);
     });
@@ -19,6 +21,8 @@ export default async function exercises() {
         activeFilter.classList.remove("active-filter");
         e.target.classList.add("active-filter");
         activeFilter = e.target;
+        categories.classList.remove('muscles');
+        categories.classList.add('bodyParts');
         renderExercises();
         renderPages(3);
     });
@@ -37,8 +41,10 @@ export default async function exercises() {
         });
     } 
     async function renderExercises() {
+        const tempArr = [];
         await fetch(`https://energyflow.b.goit.study/api/exercises?page=${page}&limit=12`).then(val => val.json()).then(val => {
             for (let i of val.results) {
+                tempArr.push(i);
                 const svg = document.querySelector(".burger svg use").href.baseVal.split("#");
                 categories.insertAdjacentHTML(
                   'beforeend',
@@ -50,7 +56,7 @@ export default async function exercises() {
         </div>
         <p><span><svg><use href="${svg[0]}#human"></use></svg></span>${i.name[0].toUpperCase() + i.name.slice(1)}</p>
         <ul>
-            <li>Burned calories: <b>${i.burnedCalories}</b></li>
+            <li>Burned calories: <b>${i.burnedCalories}/3 min</b></li>
             <li>Body part: <b>${i.bodyPart[0].toUpperCase() + i.bodyPart.slice(1)}</b></li>
             <li>Target: <b>${i.target[0].toUpperCase() + i.target.slice(1)}</b></li>
         </ul>
@@ -58,6 +64,33 @@ export default async function exercises() {
                 );
             }
         });
+        const start = document.querySelectorAll(".exercises-card .top p");
+        for (let i = 0; i < start.length;i++) {
+            start[i].addEventListener("click", () => {
+                const backdrop = document.querySelector(".backdrop");
+                const ratingList = backdrop.querySelector(".wrapper > span ul");
+                const stars = ratingList.querySelectorAll("svg");
+                for (let j = 0; j < 5; j++){
+                    stars[j].classList.remove("rated");
+                }
+                for (let j = 0; j < Math.floor(tempArr[i].rating); j++){
+                    stars[j].classList.add("rated");
+                }
+                backdrop.classList.remove("visibility");
+                document.querySelector(".modal > svg").addEventListener("click", () => {backdrop.classList.add("visibility");});
+                backdrop.querySelector('.image').style =`background: linear-gradient(0deg, rgba(27, 27, 27, 0.20) 0%, rgba(27, 27, 27, 0.20) 100%), url(${tempArr[i].gifUrl}) lightgray -7.072px -25.893px / 107.482% 121.729% no-repeat;`;
+                backdrop.querySelector("h3").textContent = `${tempArr[i].name[0].toUpperCase() + tempArr[i].name.slice(1)}`;
+                const info = backdrop.querySelectorAll(".info li b");
+                info[0].textContent = `${tempArr[i].target[0].toUpperCase() + tempArr[i].target.slice(1)}`;
+                info[1].textContent = `${tempArr[i].bodyPart[0].toUpperCase() + tempArr[i].bodyPart.slice(1)}`;
+                info[2].textContent = `${tempArr[i].equipment[0].toUpperCase() + tempArr[i].equipment.slice(1)}`;
+                info[3].textContent = `${tempArr[i].popularity}`;
+                info[4].textContent = `${tempArr[i].burnedCalories}/3 mins`;
+                backdrop.querySelector(".description").textContent = `${tempArr[i].description}`;
+                backdrop.querySelector(".wrapper > span").textContent = `${tempArr[i].rating}`;
+                backdrop.querySelector('.wrapper > span').appendChild(ratingList);
+            });
+        }
     }
     activeFilter = document.querySelector('.filters').children[0];
     renderMuscles();
