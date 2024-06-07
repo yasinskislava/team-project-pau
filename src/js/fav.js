28049a6a5e12918eab50478435dbab8c0a171f47
yasinskislava <yasinskislava@gmail.com>
2024-06-08 01:20:59 +0300

    save

--- src/js/fav.js
+++ src/js/fav.js
@@ -61,52 +61,7 @@ headerFunc();
               cardToRemove.remove(); 
           });
       }
-      const start = document.querySelectorAll(
-        '.favorites .exercises-card .top p'
-      );
-      for (let i = 0; i < start.length; i++) {
-        start[i].addEventListener('click', () => {
-          const backdrop = document.querySelector('.backdrop');
-          const ratingList = backdrop.querySelector('.wrapper > span ul');
-          const bin = ratingList.querySelectorAll('svg');
-          for (let j = 0; j < 5; j++) {
-            bin[j].classList.remove('rated');
-          }
-          console.log(favArr);
-          for (let j = 0; j < Math.floor(favArr[i].rating); j++) {
-            bin[j].classList.add('rated');
-          }
-          backdrop.classList.remove('visibility');
-          document.querySelector('.modal > svg').addEventListener('click', () => {
-            backdrop.classList.add('visibility');
-          });
-          backdrop.querySelector(
-            '.image'
-          ).style = `background: linear-gradient(0deg, rgba(27, 27, 27, 0.20) 0%, rgba(27, 27, 27, 0.20) 100%), url(${favArr[i].gifUrl}) lightgray -7.072px -25.893px / 107.482% 121.729% no-repeat;`;
-          backdrop.querySelector('h3').textContent = `${
-            favArr[i].name[0].toUpperCase() + favArr[i].name.slice(1)
-          }`;
-          const info = backdrop.querySelectorAll('.info li b');
-          info[0].textContent = `${
-            favArr[i].target[0].toUpperCase() + favArr[i].target.slice(1)
-          }`;
-          info[1].textContent = `${
-            favArr[i].bodyPart[0].toUpperCase() + favArr[i].bodyPart.slice(1)
-          }`;
-          info[2].textContent = `${
-            favArr[i].equipment[0].toUpperCase() + favArr[i].equipment.slice(1)
-          }`;
-          info[3].textContent = `${favArr[i].popularity}`;
-          info[4].textContent = `${favArr[i].burnedCalories}/3 mins`;
-          backdrop.querySelector(
-            '.description'
-            )
-            backdrop.querySelector(
-              '.wrapper > span'
-              )
-          backdrop.querySelector('.wrapper > span').appendChild(ratingList);
-        });
-      }
+      
     }
    
 
